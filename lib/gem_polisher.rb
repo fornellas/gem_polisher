e1e4a752aec37a3d1278ef1198bb0d5c365d72dd
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2016-01-19 20:36:18 -0200

    #initialize

--- lib/gem_polisher.rb
+++ lib/gem_polisher.rb
@@ -1,2 +1,63 @@
+require 'rake'
+
 class GemPolisher
+
+  include Rake::DSL
+
+  # String representing Gem's main constant.
+  attr_reader :gem_main_constant_s
+  # Command used to publish Gem.
+  attr_reader :gem_publish_command
+
+  # This should be called from within Rakefile. It will create necessary Rake tasks.
+  # Optionally, a Hash can be passed with:
+  # +:gem_main_constant_s+:: By default, conventions documented at http://guides.rubygems.org/name-your-gem/ are followed to extract Gem's main constant name, but with strict camel case (eg: Rdoc no RDoc). Use this option case your gem does not follow strict camel case. Eg: for Gem named +net-http-persistent+, use +"Net::HTTP::Persistent"+ here.
+  # +:gem_publish_command+:: Command to use to publish your gem (eg. "gem inabox"). Default: "gem push".
+  def initialize opts={}
+    unless ENV["BUNDLE_GEMFILE"]
+      raise RuntimeError.new("#{self.class.to_s} should not be called outside Bundler environment. Try calling rake with 'bundle exec rake'")
+    end
+    @gem_main_constant_s = opts.fetch(:gem_main_constant_s) do
+      default_gem_main_constant_s
+    end
+    @gem_publish_command = opts.fetch(:gem_publish_command) { 'gem push' }
+    define_rake_tasks
+  end
+
+  private
+
+  # Returns Gem name.
+  def gem_name
+  end
+
+  # Returns String with name of Gem's main constant, calculated from #gem_name, following conventions at: http://guides.rubygems.org/name-your-gem/
+  def default_gem_main_constant_s
+    const = gem_name
+      .capitalize
+      .split('_')
+      .map{|n| n.capitalize}
+      .join('')
+    if const.match(/-/)
+      const
+        .split('-')
+        .map do |n|
+           n[0].capitalize +
+            n[1, n.length]
+         end
+        .join('::')
+    else
+      const
+    end
+  end
+
+  # Defire Rake tasks
+  def define_rake_tasks
+    namespace :gem do
+      desc 'Tag, build and publish a Gem; type can be major, minor or patch.'
+      task :release, [:type] => [:test] do |t, args|
+        raise 'TODO'
+      end
+    end
+  end
+
 end
--- spec/lib/gem_polisher_spec.rb
+++ spec/lib/gem_polisher_spec.rb
@@ -1,11 +1,64 @@
+require 'gem_polisher'
+require 'rake'
+
 RSpec.describe GemPolisher do
+  let(:gem_main_constant_s) { 'Net::Http::DigestAuth' }
+  before(:example) do
+    allow_any_instance_of(described_class)
+      .to receive(:default_gem_main_constant_s).and_return(gem_main_constant_s)
+  end
+  around(:example) do |example|
+    old_bundle_gemfile = ENV["BUNDLE_GEMFILE"]
+    ENV["BUNDLE_GEMFILE"] = 'Gemfile'
+    example.call
+    ENV["BUNDLE_GEMFILE"] = old_bundle_gemfile
+  end
   context '#initialize' do
     context 'attributes' do
-      it 'accepts custom publish command'
-      it 'accepts custom Gem constant'
+      shared_examples :initialize_attributes do
+        subject { described_class.new }
+        it { is_expected.to have_attributes(attribute => default_value) }
+        context 'passed to initialize' do
+          subject do
+            described_class.new(attribute => initialize_value)
+          end
+          it { is_expected.to have_attributes(attribute => initialize_value) }
+        end
+      end
+      context '#gem_main_constant_s' do
+        let(:attribute) { :gem_main_constant_s }
+        let(:default_value) { gem_main_constant_s }
+        let(:initialize_value) { 'Net::HTTP::DigestAuth' }
+        include_examples :initialize_attributes
+      end
+      context '#gem_publish_command' do
+        let(:attribute) { :gem_publish_command }
+        let(:default_value) { 'gem push' }
+        let(:initialize_value) { 'gem inabox' }
+        include_examples :initialize_attributes
+      end
     end
     context 'Rake tasks creation' do
-      it 'creates Rake tasks'
+      before(:example) do
+        Rake.application = Rake::Application.new
+      end
+      it 'creates gem:release[type]' do
+        described_class.new
+        expect(Rake::Task['gem:release']).to be_a(Rake::Task)
+      end
+    end
+    context 'without bundler environment' do
+      around(:example) do |example|
+        old_bundle_gemfile = ENV["BUNDLE_GEMFILE"]
+        ENV.delete "BUNDLE_GEMFILE"
+        example.call
+        ENV["BUNDLE_GEMFILE"] = old_bundle_gemfile
+      end
+      it 'fails' do
+        expect do
+          described_class.new
+        end.to raise_error(RuntimeError)
+      end
     end
   end
   context 'Rake tasks' do
@@ -18,6 +71,14 @@ RSpec.describe GemPolisher do
         # gem_build
         # gem_publish
       end
+    end
+  end
+  describe 'private methods' do
+    describe '#gem_name' do
+      it 'extracts it from .gemspec'
+    end
+    describe '#default_gem_main_constant_s' do
+      it 'works in all cases'
     end
   end
 end
